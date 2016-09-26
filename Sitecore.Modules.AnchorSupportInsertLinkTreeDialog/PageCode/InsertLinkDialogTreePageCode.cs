b1b2810ff6400702355c846c47555d7dbebc4c75
Dražen Janjiček <drazen.janjicek@hinttech.com>
2016-09-26 13:21:52 +0200

    Add project files.

---
+++ Sitecore.Modules.AnchorSupportInsertLinkTreeDialog/PageCode/InsertLinkDialogTreePageCode.cs
@@ -0,0 +1,177 @@
+﻿namespace Sitecore.Modules.AnchorSupportInsertLinkTreeDialog.PageCode
+{
+    using System;
+    using System.Net;
+    using System.Xml.Linq;
+    using Configuration;
+    using Data.Items;
+    using Diagnostics;
+    using Mvc.Presentation;
+    using Shell;
+    using Speak.Applications;
+    using Web;
+    using Web.PageCodes;
+
+    public class InsertLinkDialogTreePageCode : PageCodeBase
+    {
+        #region Original Implementation
+
+        private string targetActiveBrowserItemId = "{C5FA4571-37B3-472C-BDA1-0FADC2D2EFA7}";
+        private string targetCustomItemId = "{07CF2A84-9C22-4E85-8F3F-C301AADF5218}";
+        private string targetNewBrowserItemId = "{02A6C72E-17BB-48C5-8D35-AF9C494ED6BA}";
+
+        private static string GetXmlAttributeValue(XElement element, string attrName)
+        {
+            if (element.Attribute(attrName) == null)
+            {
+                return string.Empty;
+            }
+            return element.Attribute(attrName).Value;
+        }
+
+        public override void Initialize()
+        {
+            string setting = Settings.GetSetting("BucketConfiguration.ItemBucketsEnabled");
+            this.ListViewToggleButton.Parameters["IsVisible"] = setting;
+            this.TreeViewToggleButton.Parameters["IsVisible"] = setting;
+            this.TreeView.Parameters["ShowHiddenItems"] = UserOptions.View.ShowHiddenItems.ToString();
+            this.ReadQueryParamsAndUpdatePlaceholders();
+        }
+
+        #endregion
+
+        public Rendering Anchor { get; set; }
+
+        private void ReadQueryParamsAndUpdatePlaceholders()
+        {
+            #region Original Implementation
+
+            string queryString = WebUtil.GetQueryString("ro");
+            string str2 = WebUtil.GetQueryString("hdl");
+            if (!string.IsNullOrEmpty(queryString) && (queryString != "{0}"))
+            {
+                this.TreeView.Parameters["RootItem"] = queryString;
+            }
+            this.InsertAnchorButton.Parameters["Click"] = string.Format(this.InsertAnchorButton.Parameters["Click"], WebUtility.UrlEncode(queryString), WebUtility.UrlEncode(str2));
+            this.InsertEmailButton.Parameters["Click"] = string.Format(this.InsertEmailButton.Parameters["Click"], WebUtility.UrlEncode(queryString), WebUtility.UrlEncode(str2));
+            this.ListViewToggleButton.Parameters["Click"] = string.Format(this.ListViewToggleButton.Parameters["Click"], WebUtility.UrlEncode(queryString), WebUtility.UrlEncode(str2));
+            bool flag = str2 != string.Empty;
+            string text = string.Empty;
+
+            if (flag)
+            {
+                text = UrlHandle.Get()["va"];
+            }
+
+            if (text != string.Empty)
+            {
+                XElement element = XElement.Parse(text);
+                if (GetXmlAttributeValue(element, "linktype") == "internal")
+                {
+                    string xmlAttributeValue = GetXmlAttributeValue(element, "id");
+                    if (!string.IsNullOrWhiteSpace(xmlAttributeValue))
+                    {
+                        Item mediaItemFromQueryString = SelectMediaDialog.GetMediaItemFromQueryString(xmlAttributeValue);
+                        this.TreeView.Parameters["PreLoadPath"] = mediaItemFromQueryString.Paths.LongID.Substring(1);
+                        this.TextDescription.Parameters["Text"] = GetXmlAttributeValue(element, "text");
+                        this.AltText.Parameters["Text"] = GetXmlAttributeValue(element, "title");
+                        this.StyleClass.Parameters["Text"] = GetXmlAttributeValue(element, "class");
+                        this.QueryString.Parameters["Text"] = GetXmlAttributeValue(element, "querystring");
+                        this.SetupTargetDropbox(element);
+
+                        #endregion
+
+                        this.Anchor.Parameters["Text"] = GetXmlAttributeValue(element, "anchor");
+                    }
+                }
+            }
+        }
+
+        #region Original Implementation
+
+        private void SetupTargetDropbox(XElement fieldContent)
+        {
+            string targetNewBrowserItemId;
+            string xmlAttributeValue = GetXmlAttributeValue(fieldContent, "target");
+            if (xmlAttributeValue.Equals("_blank", StringComparison.OrdinalIgnoreCase))
+            {
+                targetNewBrowserItemId = this.TargetNewBrowserItemId;
+            }
+            else if (string.IsNullOrWhiteSpace(xmlAttributeValue))
+            {
+                targetNewBrowserItemId = this.TargetActiveBrowserItemId;
+            }
+            else
+            {
+                targetNewBrowserItemId = this.TargetCustomItemId;
+                this.CustomUrl.Parameters["Text"] = xmlAttributeValue;
+            }
+            this.TargetLoadedValue.Parameters["Text"] = targetNewBrowserItemId;
+        }
+
+        public Sitecore.Mvc.Presentation.Rendering AltText { get; set; }
+
+        public Sitecore.Mvc.Presentation.Rendering CustomUrl { get; set; }
+
+        public Sitecore.Mvc.Presentation.Rendering InsertAnchorButton { get; set; }
+
+        public Sitecore.Mvc.Presentation.Rendering InsertEmailButton { get; set; }
+
+        public Sitecore.Mvc.Presentation.Rendering ListViewToggleButton { get; set; }
+
+        public Sitecore.Mvc.Presentation.Rendering QueryString { get; set; }
+
+        public Sitecore.Mvc.Presentation.Rendering StyleClass { get; set; }
+
+        public Sitecore.Mvc.Presentation.Rendering Target { get; set; }
+
+        public string TargetActiveBrowserItemId
+        {
+            get
+            {
+                return this.targetActiveBrowserItemId;
+            }
+            set
+            {
+                Assert.ArgumentNotNull(value, "value");
+                this.targetActiveBrowserItemId = value;
+            }
+        }
+
+        public string TargetCustomItemId
+        {
+            get
+            {
+                return this.targetCustomItemId;
+            }
+            set
+            {
+                Assert.ArgumentNotNull(value, "value");
+                this.targetCustomItemId = value;
+            }
+        }
+
+        public Sitecore.Mvc.Presentation.Rendering TargetLoadedValue { get; set; }
+
+        public string TargetNewBrowserItemId
+        {
+            get
+            {
+                return this.targetNewBrowserItemId;
+            }
+            set
+            {
+                Assert.ArgumentNotNull(value, "value");
+                this.targetNewBrowserItemId = value;
+            }
+        }
+
+        public Sitecore.Mvc.Presentation.Rendering TextDescription { get; set; }
+
+        public Sitecore.Mvc.Presentation.Rendering TreeView { get; set; }
+
+        public Sitecore.Mvc.Presentation.Rendering TreeViewToggleButton { get; set; }
+
+        #endregion
+    }
+}
---
+++ Sitecore.Modules.AnchorSupportInsertLinkTreeDialog/sitecore/shell/client/Speak/Layouts/Renderings/Resources/Rules/ConditionsAndActions/Actions/MakeInternalLinkFromTreeView.js
@@ -0,0 +1,80 @@
+﻿define([], function () {
+    var action = function (context, args) {
+        var targetDisplayTextID = context.app[args.targetDisplayTextID],
+          targetPathID = context.app[args.targetPathID],
+          targetPathProperty = args.targetPathProperty,
+          targetQueryID = context.app[args.targetQueryID],
+          targetAltTextID = context.app[args.targetAltTextID],
+          targetStyleID = context.app[args.targetStyleID],
+          targetWindowID = context.app[args.targetWindowID],
+          customUrlID = context.app[args.customUrlID],
+
+          // Define a variable called targetAnchorID (this name we used in our rule definition) and 
+          // assign it the value of context.app[args.targetAnchorID]
+          targetAnchorID = context.app[args.targetAnchorID],
+
+          targetControlID = context.app[args.targetControlID],
+            selectedItemsPropertyName = "selectedNode",
+
+          // Extend the value of the template variable with an anchor attribute
+          template = '<link anchor="<%= anchor %>" text="<%=displayText%>" linktype="internal" class="<%=styleClass%>" title="<%=alternateText%>" <%=target%> querystring="<%=queryString%>" id="<%=itemId%>" />',
+
+          targetWindowValue, itemLink, path,
+          emptyOptionID = "{A3C9DB39-1D1B-4AA1-8C68-7B9674D055EE}",
+          htmlEncode = function (str) {
+              return str.toString().replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+          };
+
+        if (!targetDisplayTextID) {
+            console.log("Provide at least display text for your link");
+            return false;
+        }
+
+        targetWindowValue = targetWindowID.get("selectedItem");
+
+        if (!targetWindowValue || targetWindowValue.itemId === emptyOptionID) {
+            targetWindowValue = "";
+        } else {
+            var targetWindow = targetWindowValue.$displayName.trim();
+
+            switch (targetWindow) {
+                case 'Active Browser':
+                    targetWindow = "";
+                    break;
+                case 'New Browser':
+                    targetWindow = "_blank";
+                    break;
+                case "Custom":
+                    targetWindow = customUrlID.get("text");
+                    break;
+                default:
+                    targetWindow = "";
+            }
+
+            targetWindowValue = "target=\"" + targetWindow + "\"";
+        }
+
+        if (targetPathID.get(selectedItemsPropertyName) &&
+              "rawItem" in targetPathID.get(selectedItemsPropertyName) &&
+              targetPathID.get(selectedItemsPropertyName).rawItem[targetPathProperty]) {
+            path = targetPathID.get(selectedItemsPropertyName).rawItem[targetPathProperty];
+        }
+
+        itemLink = _.template(template, {
+            displayText: htmlEncode(targetDisplayTextID.get("text")),
+            alternateText: htmlEncode(targetAltTextID.get("text")),
+            itemId: targetControlID.get("selectedItemId"),
+            queryString: htmlEncode(targetQueryID.get("text")),
+            target: targetWindowValue,
+            styleClass: targetStyleID.get("text"),
+            path: path,
+
+            // Store the anchor value retrieved from the dialog in the anchor attribute
+            anchor: targetAnchorID.get("text")
+        });
+
+        context.app.closeDialog(itemLink);
+    };
+
+    return action;
+});
